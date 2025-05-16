f7ff09305d69a5dd84e9adb73030b8ba373359ca
hamza taj alden <htaj530@gmail.com>
2025-05-17 00:34:24 +0300

    change Locale languge

--- lib/core/localization/translation.dart
+++ lib/core/localization/translation.dart
@@ -2,12 +2,5 @@ import 'package:get/get.dart';
 
 class TranslationsApp extends Translations {
   @override
-  Map<String, Map<String, String>> get keys => {
-        'en': {
-        
-        },
-        'ar': {
-      
-        },
-      };
+  Map<String, Map<String, String>> get keys => {"ar": {"1":"hello"}, "en": {"1":"hello"}};
 }
